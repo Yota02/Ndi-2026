740ba8bc8a03532b74c43e44c6369ce539ac5c30
depinfo <julien.garcia05@etu.umontpellier.fr>
2025-12-05 03:12:23 +0100

    feat: ajout page sobriété

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TrailerPage from '../views/TrailerPage.vue'
 import HomePage from '../views/HomeView.vue'
+import Sobriete from '@/views/SobrieteView.vue'
 
 const SESSION_KEY = 'trailer_seen'
 
@@ -16,6 +17,11 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomePage
+    },
+    {
+      path: '/sobriete-numerique',
+      name: 'sobriete-numerique',
+      component: Sobriete,
     }
   ]
 })
