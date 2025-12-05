eaa6c1afe27bad121a124e43dfc417691acf0192
michauxa <alexis.michaux-kinet@etu.umontpellier.fr>
2025-12-05 06:33:09 +0100

    fix: phone game route

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -58,6 +58,9 @@ const closeMenu = () => {
           >
         </li>
         <li>
+          <RouterLink to="/phone-game" class="navbar-link" @click="closeMenu">Cycle de vie Smartphone</RouterLink>
+        </li>
+        <li>
           <RouterLink to="/contact" class="navbar-link" @click="closeMenu">Contact</RouterLink>
         </li>
       </ul>
